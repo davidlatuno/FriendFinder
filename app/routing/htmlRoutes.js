e722bbb4406446876ec18a8350efdc4235597da1
davidlatuno <dslatuno@gmail.com>
2018-04-18 13:59:58 -0700

    Updated modular routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,8 @@
+var path = require("path");
+
 function home(app) {
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "/app/public/home.html"));
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
 }
 module.exports = home;
--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
-var htmlRoutes = require("./app/routing/htmlRoutes.js");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
-});
-
-// htmlRoutes(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
