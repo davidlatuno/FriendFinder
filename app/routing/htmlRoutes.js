a436e81f0e5c4d85348ed033cedb22a99057d360
davidlatuno <dslatuno@gmail.com>
2018-04-18 20:43:36 -0700

    initial draft of survey.html and server route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,15 @@
 var path = require("path");
 
-function home(app) {
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));
-    });
+var htmlRoutes = {
+    home: function (app) {
+        app.get("/", function (req, res) {
+            res.sendFile(path.join(__dirname, "/../public/home.html"));
+        });
+    },
+    survey: function(app) {
+        app.get("/survey", function (req, res) {
+            res.sendFile(path.join(__dirname, "/../public/survey.html"));
+        });
+    }
 }
-module.exports = home;
+module.exports = htmlRoutes;
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/htmlRoutes.js").home(app);
+require("./app/routing/htmlRoutes.js").survey(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
